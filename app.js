405d2c57d96ade73168a2c016346c9eb7519991a
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-07-15 14:06:44 +0530

    Adding js file

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-// var a =10;
+var a =10;
 
 
-// function sample(){
-//     var a =20;
-// }
+function sample(){
+    var a =20;
+}
 
-// console.log(a)
+console.log(a)
 
 var div = document.getElementById("header")
 console.log(div.innerText)
