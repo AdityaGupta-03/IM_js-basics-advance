d19828a5c0a5c608d32206a43f146fcfb9232480
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-07-15 14:05:04 +0530

    Readme file

---
+++ app.js
@@ -0,0 +1,14 @@
+// var a =10;
+
+
+// function sample(){
+//     var a =20;
+// }
+
+// console.log(a)
+
+var div = document.getElementById("header")
+console.log(div.innerText)
+
+var para = document.getElementsByClassName('para')
+console.log(para[0].textContent)
